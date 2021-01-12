142cdf6989918d7248388a982dd4831bbb3c4813
jooam <ja960508@naver.com>
2021-01-12 14:02:14 +0900

    selection btn update

--- src/tutorial_section.js
+++ src/tutorial_section.js
@@ -1,1 +1,7 @@
-const lanToggler = document.querySelector
+const selection = document.querySelector('.js-tutorial-selection');
+
+function getTutorialPage(event) {
+    console.log(event);
+}
+
+selection.addEventListener('click', getTutorialPage);
